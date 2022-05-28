6ae3b382c0411a3f92be380e634c79fbffcea093
CGCavalcante <carol.gcavalcante@gmail.com>
2022-05-27 23:22:14 -0300

    selecionar cards

--- home/home.js
+++ home/home.js
@@ -1,30 +1,71 @@
 selectedBroth = '';
-selectedMeet = 'chasu';
+selectedMeet = '';
 habilitarBotao = false;
+listaBroth = {'salt': false, 'shoyu': false, 'miso': false};
+listaMeat = {'chasu': false, 'yasai_vegetarian': false, 'karaague': false}
 
 function selecionarBroth( event ) {
 
-    this.selectedBroth = event.id;
-    this.verificarItensSelecionados();
+    this.selectedBroth = event;
     
+    for ( let item in this.listaBroth) {
+        
+        if( this.listaBroth[item] ) {
+
+            document.getElementById(item + "-img").src = "./../imagem/icons/" + item + "/inactive.svg";
+            document.getElementById(item + '-card').style.backgroundColor = '#FAFAED';
+            document.getElementById(item + '-p1').style.color = '#1820EF';
+            document.getElementById(item + '-p2').style.color = '#000000';
+            document.getElementById(item + '-p3').style.color = '#FF4E42';
+            this.listaBroth[item] = false;
+            
+        }
+
+    }
+
+    document.getElementById(event + "-img").src = "./../imagem/icons/" + event + "/active.svg";
+    document.getElementById(event + '-card').style.backgroundColor = '#1820EF';
+    document.getElementById(event + '-p1').style.color = 'white';
+    document.getElementById(event + '-p2').style.color = 'white';
+    document.getElementById(event + '-p3').style.color = 'white';
+    this.listaBroth[event] = true;    
 
 }
 
-function selecionarMeet( event ) {
-    
+function selecionarMeat( event ) {
+
     this.selectedMeet = event;
-    this.verificarItensSelecionados();
     
+    for ( let item in this.listaMeat) {
+        
+        if( this.listaMeat[item] ) {
+
+            document.getElementById(item + "-img").src = "./../imagem/icons/" + item + "/inactive.svg";
+            document.getElementById(item + '-card').style.backgroundColor = '#FAFAED';
+            document.getElementById(item + '-p1').style.color = '#1820EF';
+            document.getElementById(item + '-p2').style.color = '#000000';
+            document.getElementById(item + '-p3').style.color = '#FF4E42';
+            this.listaMeat[item] = false;
+            
+        }
+
+    }
+
+    document.getElementById(event + "-img").src = "./../imagem/icons/" + event + "/active.svg";
+    document.getElementById(event + '-card').style.backgroundColor = '#1820EF';
+    document.getElementById(event + '-p1').style.color = 'white';
+    document.getElementById(event + '-p2').style.color = 'white';
+    document.getElementById(event + '-p3').style.color = 'white';
+    this.listaMeat[event] = true;    
+
 }
 
 function verificarItensSelecionados() {
 
-    if ( this.selectedBroth == '' ) {
-        this.habilitarBotao = false;
-    } else if ( this.selectedMeet == '' ) {
-        this.habilitarBotao = false;
+    if ( this.selectedBroth != '' && this.selectedMeet != '' ) {
+        this.consultarPedido();
     } else {
-        this.habilitarBotao = true;
+        alert("Select a broth and a meat, please!")
     }
     
 }
@@ -44,7 +85,6 @@ function consultarPedido() {
 
     let url = 'https://front-br-challenges.web.app/api/v1/ramen-go/?meat=' + this.selectedMeet + '&broth=' + this.selectedBroth
     let retorno = this.consultar(url);
-    console.log(retorno);
 }
 
 
